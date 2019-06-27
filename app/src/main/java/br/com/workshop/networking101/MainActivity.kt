2c7e778e13d6777caed3f2a06a788387ac1379b8
Natan Ximenes <natanximenes29@gmail.com>
2019-06-27 00:38:57 -0300

    Resolve TODOS do passo1

--- app/src/main/java/br/com/workshop/networking101/MainActivity.kt
+++ app/src/main/java/br/com/workshop/networking101/MainActivity.kt
@@ -1,9 +1,15 @@
 package br.com.workshop.networking101
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.TextView
+import br.com.workshop.networking101.utilities.NetworkUtils
+import java.io.IOException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private val searchFieldEditText by lazy { findViewById<EditText>(R.id.search_field) }
@@ -15,9 +21,52 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    //TODO - 6 Criar uma AsyncTask para realizar as requisições à API do Github.
-    //TODO - 7 Ao término da execução, setar no searchResultsTextView o retorno da requisição, caso o resultado seja válido.
-    //TODO - 8 Criar um método para executar a AsyncTask, utilizando o texto digitado no searchFieldEditText, para gerar uma URL.
+    //DONE - 9 Sobrescreva os callbacks de menu e chamar este método ao clicar no item de busca no menu.
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val itemThatWasClickedId = item.itemId
+        if (itemThatWasClickedId == R.id.action_search) {
+            makeGithubSearchQuery()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    
+
+    //DONE - 8 Criar um método para executar a AsyncTask, utilizando o texto digitado no searchFieldEditText, para gerar uma URL.
     // e exibir a URL criada com os parametros de busca no searchURLTextView.
-    //TODO - 9 Sobrescreva os callbacks de menu e chamar este método ao clicar no item de busca no menu.
+    private fun makeGithubSearchQuery() {
+        val githubQuery = searchFieldEditText.text.toString()
+        val githubSearchUrl = NetworkUtils.buildUrl(githubQuery)
+        githubSearchUrl?.let {
+            searchURLTextView.text = it.toString()
+            GithubQueryTask().execute(it)
+        }
+    }
+
+    //DONE - 6 Criar uma AsyncTask para realizar as requisições à API do Github.
+    //DONE - 7 Ao término da execução, setar no searchResultsTextView o retorno da requisição, caso o resultado seja válido.
+    inner class GithubQueryTask : AsyncTask<URL, Void, String>() {
+
+        override fun doInBackground(vararg params: URL): String? {
+            val searchUrl = params[0]
+            return try {
+                NetworkUtils.getResponseFromHttpUrl(searchUrl)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                null
+            }
+        }
+
+        override fun onPostExecute(githubSearchResults: String?) {
+            if (!githubSearchResults.isNullOrBlank()) {
+                searchResultsTextView.text = githubSearchResults
+            }
+        }
+    }
+
 }
--- app/src/main/java/br/com/workshop/networking101/utilities/NetworkUtils.kt
+++ app/src/main/java/br/com/workshop/networking101/utilities/NetworkUtils.kt
@@ -1,5 +1,12 @@
 package br.com.workshop.networking101.utilities
 
+import android.net.Uri
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.*
+
 object NetworkUtils {
 
     private const val GITHUB_BASE_URL = "https://api.github.com/search/repositories"
@@ -7,11 +14,36 @@ object NetworkUtils {
     private const val PARAM_SORT = "sort"
     private const val sortBy = "stars"
 
-    //TODO - 1 Criar método para receber a string de busca e retornar um objeto do tipo URL
-    //TODO - 2 Construa a URL utilizando as constantes de query parameters e a base url acima.
+    //DONE - 1 Criar método para receber a string de busca e retornar um objeto do tipo URL
+    //DONE - 2 Construa a URL utilizando as constantes de query parameters e a base url acima.
+    fun buildUrl(githubSearchQuery: String): URL? {
+        val builtUri = Uri.parse(GITHUB_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_QUERY, githubSearchQuery)
+                .appendQueryParameter(PARAM_SORT, sortBy)
+                .build()
 
-    //TODO - 3 Criar método que receba uma URL e:
-    //TODO - 4 abra uma conexão HTTP
-    //TODO - 5 recupere o retorno da requisição e retorne como string
+        return try {
+            URL(builtUri.toString())
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+            null
+        }
+    }
 
+    //DONE - 3 Criar método que receba uma URL e:
+    //DONE - 4 abra uma conexão HTTP
+    //DONE - 5 recupere o retorno da requisição e retorne como string
+    @Throws(IOException::class)
+    fun getResponseFromHttpUrl(url: URL): String? {
+        val urlConnection = url.openConnection() as HttpURLConnection
+        try {
+            val inputStream = urlConnection.inputStream
+            val scanner = Scanner(inputStream)
+            scanner.useDelimiter("\\A")
+            val hasInput = scanner.hasNext()
+            return if (hasInput) scanner.next() else null
+        } finally {
+            urlConnection.disconnect()
+        }
+    }
 }
