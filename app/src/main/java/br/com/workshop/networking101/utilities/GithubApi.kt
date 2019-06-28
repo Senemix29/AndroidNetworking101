228e211c47b3475accaaf7d8421f1cb1a0696460
Natan Ximenes <natanximenes29@gmail.com>
2019-06-28 01:26:42 -0300

    Implementa a criação da instancia do serviço de busca dentro do singleton

--- app/src/main/java/br/com/workshop/networking101/utilities/GithubApi.kt
+++ app/src/main/java/br/com/workshop/networking101/utilities/GithubApi.kt
@@ -4,21 +4,24 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object GithubApi {
-    private const val GITHUB_BASE_URL = "https://api.github.com/search/repositories"
-    private var retrofit: Retrofit? = null
     const val PARAM_QUERY = "q"
     const val PARAM_SORT = "sort"
     const val SORT_BY_STARS = "stars"
-
-    fun getInstance() = retrofit ?: buildRetrofit()
+    private const val GITHUB_BASE_URL = "https://api.github.com/search/repositories"
+    lateinit var searchService: GithubSearchService
+    init {
+        buildRetrofit()
+    }
 
     private fun buildRetrofit() {
-        retrofit = Retrofit
+        val retrofit = Retrofit
                 .Builder()
                 .baseUrl(GITHUB_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
-        retrofit?.create(GithubSearchService::class.java)
+        searchService = retrofit.create(GithubSearchService::class.java)
     }
+
+
 }
