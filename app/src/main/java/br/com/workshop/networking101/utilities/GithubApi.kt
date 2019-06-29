2c14027e4aabcc6377cde5fc83670b01beae9e5a
Natan Ximenes <natanximenes29@gmail.com>
2019-06-29 02:13:24 -0300

    Personaliza o client do okhttp

--- app/src/main/java/br/com/workshop/networking101/utilities/GithubApi.kt
+++ app/src/main/java/br/com/workshop/networking101/utilities/GithubApi.kt
@@ -1,15 +1,24 @@
 package br.com.workshop.networking101.utilities
 
+import br.com.workshop.networking101.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level.NONE
+import okhttp3.logging.HttpLoggingInterceptor.Level.BODY
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object GithubApi {
     private const val GITHUB_BASE_URL = "https://api.github.com/"
-    const val PARAM_QUERY = "q"
-    const val PARAM_SORT = "sort"
-    const val SORT_BY_STARS = "stars"
+    private const val AUTH_HEADER_KEY = "Authorization"
+    private const val AUTH_TOKEN = "token 6be68c0ed3b3e7d1cb1a2d66c6e752635ce1632c"
+
+    private lateinit var okHttpClient: OkHttpClient
     lateinit var searchService: GithubSearchService
+
     init {
+        buildOkHttpClient()
         buildRetrofit()
     }
 
@@ -17,11 +26,31 @@ object GithubApi {
         val retrofit = Retrofit
                 .Builder()
                 .baseUrl(GITHUB_BASE_URL)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
         searchService = retrofit.create(GithubSearchService::class.java)
     }
 
+    private fun buildOkHttpClient() {
+        val authInterceptor = Interceptor {
+            val request = it
+                    .request()
+                    .newBuilder()
+                    .addHeader(AUTH_HEADER_KEY, AUTH_TOKEN)
+                    .build()
+            it.proceed(request)
+        }
+
+        val logginInterceptor = HttpLoggingInterceptor()
+        logginInterceptor.level = if (BuildConfig.DEBUG) BODY else NONE
+
+        okHttpClient = OkHttpClient
+                .Builder()
+                .addInterceptor(authInterceptor)
+                .addInterceptor(logginInterceptor)
+                .build()
+    }
 
 }
--- app/src/main/java/br/com/workshop/networking101/utilities/GithubSearchService.kt
+++ app/src/main/java/br/com/workshop/networking101/utilities/GithubSearchService.kt
@@ -1,15 +1,17 @@
 package br.com.workshop.networking101.utilities
 
-import br.com.workshop.networking101.utilities.GithubApi.PARAM_QUERY
-import br.com.workshop.networking101.utilities.GithubApi.PARAM_SORT
-import br.com.workshop.networking101.utilities.GithubApi.SORT_BY_STARS
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface GithubSearchService {
-
     @GET("search/repositories")
     fun searchRespositories(@Query(PARAM_QUERY) query: String,
                             @Query(PARAM_SORT) sortBy: String = SORT_BY_STARS): Call<SearchResult>
+
+    companion object {
+        const val PARAM_QUERY = "q"
+        const val PARAM_SORT = "sort"
+        const val SORT_BY_STARS = "stars"
+    }
 }
